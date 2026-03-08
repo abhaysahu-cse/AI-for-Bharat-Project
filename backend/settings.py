d5dbc6f0601ea2484ca571b84991796a739e85be
Abhay Sahu <abhaysahu.cse@gmail.com>
2026-03-08 12:42:42 +0530

    Fix Railway port + migrations

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-l+#-n67r^x2zypa&83bvz!d#5kl1r2%ulle@kkaid^7kxb3^^o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 
